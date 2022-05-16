fb4702c6a95244d292600831961160e917c263df
Jose Maza <jgmaza@uninorte.edu.co>
2022-05-16 11:07:49 -0500

    TodoItem updated

--- src/components/TodoItem.jsx
+++ src/components/TodoItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 
-export default function TodoItem({ todo }) {
+export default function TodoItem({ todo ,toggleTodo}) {
     const { id, task, completed } = todo
 
     const handleTodoClick = () => {
